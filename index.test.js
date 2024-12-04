edf675cb47a4c007f8227e5cc01eb5a4ba6d4746
Matheus Melo <dev.matheus.melo@gmail.com>
2024-12-04 14:07:32 -0300

    feat: :sparkles: add integration with sonarqube

--- index.test.js
+++ index.test.js
@@ -1,6 +1,13 @@
-const soma = require('./index.js')
-const { deepStrictEqual } = require('assert')
+const soma = require("./index.js");
+const { deepStrictEqual } = require("assert");
+const { test } = require("node:test");
 
-;(async () => {
-    deepStrictEqual(30, soma(15, 15))
-})()
+/**
+ * Node v22.11.0 LTS
+ * node --test --experimental-test-coverage --test-reporter=lcov --test-reporter-destination=lcov.info
+ * Gerar arquivo lcov.info para integração com sonar scanner
+ */
+
+test("teste da funcao soma", () => {
+	deepStrictEqual(30, soma(15, 15));
+});
