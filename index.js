913bed9d1fe7fb87f0f3fcb62565cd6467ee5a61
Matheus Melo <dev.matheus.melo@gmail.com>
2024-12-10 13:47:32 -0300

    feat: add new functions

--- index.js
+++ index.js
@@ -18,10 +18,36 @@ function potencia(a, b) {
 	return Math.pow(a, b);
 }
 
+function dobra(a) {
+	return a * 2;
+}
+
+function triplica(a) {
+	return a * 3;
+}
+
+function adiciona100(a) {
+	return a + 100;
+}
+
+function adiciona200(a) {
+	return a + 200;
+}
+
+function adiciona300(a) {
+	return a + 300;
+}
+
 module.exports = {
 	soma,
 	multiplicacao,
 	subtracao,
 	potencia,
 	divisao,
+	adiciona100,
+	adiciona200,
+	adiciona300,
+	dobra,
+	potencia,
+	triplica,
 };
