fba4a403e26332f88437a0b7d926af8395ab84e0
Matheus Melo <dev.matheus.melo@gmail.com>
2024-12-10 12:30:00 -0300

    chore: commit to test quality gate

--- index.js
+++ index.js
@@ -18,6 +18,26 @@ function potencia(a, b) {
 	return Math.pow(a, b);
 }
 
+function dobra(a) {
+	return a * 2;
+}
+
+function triplica(a) {
+	return a * 3;
+}
+
+function adiciona100(a) {
+	return a + 100;
+}
+
+function adiciona200(a) {
+	return a + 200;
+}
+
+function adiciona300(a) {
+	return a + 300;
+}
+
 module.exports = {
 	soma,
 	multiplicacao,
